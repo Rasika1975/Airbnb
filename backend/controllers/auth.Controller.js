2c621db52fc28b55c3e5a46e0683d3bdb7270d48
@Rasika1975 <rasikagudulkar1008@gmail.com>
2026-01-23 16:42:18 +0530

    Update cookie settings for authentication

--- backend/controllers/auth.Controller.js
+++ backend/controllers/auth.Controller.js
@@ -68,8 +68,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
 
@@ -90,8 +90,8 @@ export const logout = async (req, res) => {
     try {
         res.clearCookie("token", {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict"
+            secure: true,
+            sameSite: "none",
         });
 
         return res.status(200).json({ message: "Logout successful" });
